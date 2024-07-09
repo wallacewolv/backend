4b256e0dbbd69f5067d02d6a68e1544d62095130
wallace <wallace_wolv@hotmail.com>
2024-07-08 23:00:47 -0300

    feat: Rota createTrip

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,9 @@
 import fastify from "fastify";
-import { prisma } from './lib/prisma';
+import { createTrip } from "./routes/create-trip";
 
 const app = fastify()
 
-app.get('/cadastrar', async () => {
-    await prisma.trip.create({
-        data: {
-            destination: 'Florianópolis',
-            starts_at: new Date(),
-            ends_at: new Date(),
-        }
-    });
-
-    return 'Registro cadastrado com sucesso!';
-})
-
-app.get('/listar', async () => {
-    const trips = await prisma.trip.findMany();
-
-    return trips;
-})
+app.register(createTrip);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!')
