e8ca06bf01327fbfb47839f51d027d395c439ed1
wallace <wallace_wolv@hotmail.com>
2024-07-08 22:49:34 -0300

    feat: Configurando criação das trips com o prisma

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import fastify from "fastify";
+import { prisma } from './lib/prisma';
 
 const app = fastify()
 
-app.get('/teste', () => {
-    return 'Hello World'
+app.get('/cadastrar', async () => {
+    await prisma.trip.create({
+        data: {
+            destination: 'Florianópolis',
+            starts_at: new Date(),
+            ends_at: new Date(),
+        }
+    });
+
+    return 'Registro cadastrado com sucesso!';
 })
 
-app.listen({port: 3333}).then(() => {
+app.get('/listar', async () => {
+    const trips = await prisma.trip.findMany();
+
+    return trips;
+})
+
+app.listen({ port: 3333 }).then(() => {
     console.log('Server running!')
 })
