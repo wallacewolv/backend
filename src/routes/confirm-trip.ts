3fff015f2d048c99e5ed6ea5585c743e157c25a0
wallace <wallace_wolv@hotmail.com>
2024-07-09 20:06:15 -0300

    feat: Criando rota de confirmação do participante

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,10 +1,10 @@
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import nodemailer from "nodemailer";
-import z from "zod";
-import { prisma } from "@/lib/prisma";
-import { dayjs } from "@/lib/dayjs";
-import { getMailClient } from "@/lib/mail";
+import { dayjs } from '@/lib/dayjs';
+import { getMailClient } from '@/lib/mail';
+import { prisma } from '@/lib/prisma';
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import nodemailer from 'nodemailer';
+import z from 'zod';
 
 export async function confirmTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', {
@@ -51,7 +51,7 @@ export async function confirmTrip(app: FastifyInstance) {
 
         await Promise.all(
             trip.participants.map(async (participant) => {
-                const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/${participant.id}`;
+                const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
 
                 const message = await mail.sendMail({
                     from: {
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipants } from "./routes/confirm-participant";
 
 const app = fastify();
 
@@ -15,7 +16,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipants);
 
 app.listen({ port: 3333 }).then(() => {
-    console.log('Server running!')
+    console.log('Server running!');
 })
