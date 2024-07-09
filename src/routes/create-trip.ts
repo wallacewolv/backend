94c4f6a422ee1f0e694714f0dc6c52c5e7a2172b
wallace <wallace_wolv@hotmail.com>
2024-07-09 16:28:44 -0300

    feat: Criando export para reutilzar o dayjs

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,15 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import dayjs from "dayjs";
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-import "dayjs/locale/pt-br";
 import nodemailer from "nodemailer";
 import z from "zod";
 import { getMailClient } from "@/lib/mail";
-
-dayjs.locale('pt-br');
-dayjs.extend(localizedFormat);
+import { dayjs } from "@/lib/dayjs";
 
 export async function createTrip(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/trips', {
