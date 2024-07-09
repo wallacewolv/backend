bf7b02732e152b1732ae6b89bcb1a4da51237aca
wallace <wallace_wolv@hotmail.com>
2024-07-08 23:17:56 -0300

    feat: Implementando a criação da trip com o prisma

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/lib/prisma";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
@@ -9,15 +10,19 @@ export async function createTrip(app: FastifyInstance) {
                 destination: z.string().min(4),
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
-            })
-        }
+            }),
+        },
     }, async (request) => {
         const { destination, starts_at, ends_at } = request.body;
 
-        return {
-            destination,
-            starts_at,
-            ends_at
-        };
+        const trip = await prisma.trip.create({
+            data: {
+                destination,
+                starts_at,
+                ends_at,
+            },
+        });
+
+        return { tripId: trip.id };
     })
 }
