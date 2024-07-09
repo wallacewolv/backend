646004f2e39773cb5a7e7097c7d262f65b1f840e
wallace <wallace_wolv@hotmail.com>
2024-07-08 21:40:11 -0300

    feat: Inciando server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/teste', () => {
+    return 'Hello World'
+})
+
+app.listen({port: 3333}).then(() => {
+    console.log('Server running!')
+})
