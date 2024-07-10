2f0ebdb43038d0d6c566bbaf90f088a9692a43b8
wallace <wallace_wolv@hotmail.com>
2024-07-09 23:58:37 -0300

    feat: Rota de criação do link

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
 
 const app = fastify();
 
@@ -21,6 +22,7 @@ app.register(confirmTrip);
 app.register(confirmParticipants);
 app.register(createActivity);
 app.register(getActivities);
+app.register(createLink);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
