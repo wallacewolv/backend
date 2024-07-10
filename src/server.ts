94f19d0a7da0cb447801b64d493747631ca03d4b
wallace <wallace_wolv@hotmail.com>
2024-07-09 23:30:06 -0300

    feat: Criando rota para criar atividade

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
 
 const app = fastify();
 
@@ -17,6 +18,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipants);
+app.register(createActivity);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
