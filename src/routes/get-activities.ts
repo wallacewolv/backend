e019a71326d109ee23032a6d37459137104fbc3b
wallace <wallace_wolv@hotmail.com>
2024-07-09 23:51:43 -0300

    refactor: Ajuste para retornar cada dia da viagem com as suas atividades associadas

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -16,13 +16,32 @@ export async function getActivities(app: FastifyInstance) {
 
         const trip = await prisma.trip.findUnique({
             where: { id: tripId },
-            include: { activities: true },
+            include: {
+                activities: {
+                    orderBy: {
+                        occurs_at: 'asc',
+                    },
+                },
+            },
         });
 
         if (!trip) {
             throw new Error('Trip not found');
         }
 
-        return { activities: trip.activities };
+        const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(trip.starts_at, 'days');
+
+        const activities = Array.from({ length: differenceInDaysBetweenTripStartAndEnd + 1 }).map((_, index) => {
+            const date = dayjs(trip.starts_at).add(index, 'days');
+
+            return {
+                date: date.toDate(),
+                activities: trip.activities.filter(activity => {
+                    return dayjs(activity.occurs_at).isSame(date, 'day')
+                }),
+            };
+        });
+
+        return { activities };
     });
 }
