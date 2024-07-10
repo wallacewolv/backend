d7451183cbe7420e2b421ea2cb7aed1560c4cd07
wallace <wallace_wolv@hotmail.com>
2024-07-09 23:43:55 -0300

    feat: Rota para obter atividades

---
+++ src/routes/get-activities.ts
@@ -0,0 +1,28 @@
+import { dayjs } from '@/lib/dayjs';
+import { prisma } from '@/lib/prisma';
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import z from 'zod';
+
+export async function getActivities(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/activities', {
+        schema: {
+            params: z.object({
+                tripId: z.string().uuid(),
+            })
+        },
+    }, async (request) => {
+        const { tripId } = request.params;
+
+        const trip = await prisma.trip.findUnique({
+            where: { id: tripId },
+            include: { activities: true },
+        });
+
+        if (!trip) {
+            throw new Error('Trip not found');
+        }
+
+        return { activities: trip.activities };
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
 
 const app = fastify();
 
@@ -19,6 +20,7 @@ app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipants);
 app.register(createActivity);
+app.register(getActivities);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
