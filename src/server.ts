2f577df2e8af777657d394f0bd9dc4e648de1f96
wallace <wallace_wolv@hotmail.com>
2024-07-10 21:43:10 -0300

    feat: Rota para pbter detalhes da viagem

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
 import { createInvite } from "./routes/create-invite";
 import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
 
 const app = fastify();
 
@@ -31,6 +32,7 @@ app.register(getLinks);
 app.register(getParticipants);
 app.register(createInvite);
 app.register(updateTrip);
+app.register(getTripDetails);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
